33bf6690232d8dfaf531aa6311a6ab942981ddc2
francisco-bootcamp <encode@mail.fmf.rocks>
2022-10-13 15:00:00 +0100

    Added wallet address on html; imported ethers to generate a random wallet

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ethers } from 'ethers';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
 
-  constructor() { }
+export class DashboardComponent implements OnInit {
+  walletAddress: string;
+
+  constructor() { 
+    this.walletAddress = 'Loading...'
+  }
 
   ngOnInit(): void {
+    const newWallet = ethers.Wallet.createRandom();
+    this.walletAddress = newWallet.address;
   }
 
 }
