7fd01ab526292196f3e69f1595b1a5394b1008d7
francisco-bootcamp <encode@mail.fmf.rocks>
2022-10-16 18:10:47 +0100

    Added delegate calls to the Blockchain, not fully tested

--- frontend/src/app/delegate/delegate.component.ts
+++ frontend/src/app/delegate/delegate.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import MyToken from 'src/assets/MyToken.json'
+import { ethers, Contract } from 'ethers';
+
+//FIXME: CHANGE address or import from env
+const MYTOKEN_CONTRACT_ADDRESS = '0xeb0170d478b723af8aed0abe4413e49b15428673';
 
 @Component({
   selector: 'app-delegate',
@@ -7,19 +12,39 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./delegate.component.scss']
 })
 export class DelegateComponent implements OnInit {
+  wallet: ethers.Wallet | undefined;
+  provider: ethers.providers.BaseProvider | undefined;
+  myTokenContract: Contract;
   delegateForm: FormGroup;
   
   constructor() { 
     this.delegateForm = new FormGroup({
       delegatee: new FormControl('', Validators.compose([Validators.required])),
     });
+
+    this.provider = ethers.getDefaultProvider('goerli');
+    const pkLS = localStorage.getItem('privateKey');
+    this.wallet = ethers.Wallet.createRandom();
+    if(pkLS != null) {
+      this.wallet = new ethers.Wallet(pkLS, this.provider);
+    }
+    this.myTokenContract = new ethers.Contract(
+      MYTOKEN_CONTRACT_ADDRESS,
+      MyToken.abi,
+      this.wallet,
+    );
   }
 
   ngOnInit(): void {
   }
 
-  delegateToAddress(params: FormGroup) {
-
+  async delegateToAddress(params: FormGroup) {
+    console.log(params.value);
+    const delegatee = params.value.delegatee;
+    
+    const delegateTx = await this.myTokenContract['delegate'](delegatee);
+    const delegateReceipt = await delegateTx.wait();
+    console.log(delegateReceipt);
   }
 
 }
