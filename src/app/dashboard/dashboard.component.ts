36cb5c6e7879cbba6f2ffc6d04347a7445e60110
Steo <palutz@users.noreply.github.com>
2022-10-16 21:22:43 +0100

    readBalance and setInterval

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,15 +7,33 @@ import { ethers } from 'ethers';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  wallet : ethers.Wallet | undefined;
+  provider : ethers.providers.BaseProvider | undefined;
   walletAddress : string;
+  balance : string;
 
   constructor() {
     this.walletAddress = "Loading..."
+    this.balance = "Loading..."
+  }
+
+  readBalance () {
+    if(this.provider?._isProvider) { // check if the provider is initialised and not undefined
+      this.provider.getBalance(this.walletAddress).then(
+        bal => { this.balance = ethers.utils.formatEther(bal); } 
+      );
+    }
   }
 
   ngOnInit(): void {
-    const newWallet = ethers.Wallet.createRandom();
-    this.walletAddress = newWallet.address;
+    this.provider = ethers.getDefaultProvider("goerli");
+    this.wallet = ethers.Wallet.createRandom();
+    this.walletAddress = this.wallet.address;
+    this.readBalance();
+    // and now setting up an automatic update
+    setInterval (() => {
+      this.readBalance();
+    }, 5000);
   }
 
 }
