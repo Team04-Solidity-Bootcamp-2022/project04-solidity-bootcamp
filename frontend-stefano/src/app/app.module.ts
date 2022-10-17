a594efb973916d0b2ea44e79f5dd07baab4ca5bd
francisco-bootcamp <encode@mail.fmf.rocks>
2022-10-17 17:31:32 +0100

    Importing stefano's work

---
+++ frontend-stefano/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent, DashboardComponent],
+  imports: [BrowserModule, NgbModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ frontend-stefano/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { ethers } from 'ethers';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+  wallet : ethers.Wallet | undefined;
+  provider : ethers.providers.BaseProvider | undefined;
+  walletAddress : string;
+  balance : string;
+
+  constructor() {
+    this.walletAddress = "Loading..."
+    this.balance = "Loading..."
+  }
+
+  readBalance () {
+    if(this.provider?._isProvider) { // check if the provider is initialised and not undefined
+      this.provider.getBalance(this.walletAddress).then(
+        bal => { this.balance = ethers.utils.formatEther(bal); },
+        err => { console.error(err) }
+      );
+    }
+  }
+
+  ngOnInit(): void {
+    this.provider = ethers.getDefaultProvider("goerli");
+    this.wallet = ethers.Wallet.createRandom();
+    this.walletAddress = this.wallet.address;
+    this.readBalance();
+    // and now setting up an automatic update
+    setInterval (() => {
+      this.readBalance();
+    }, 5000);
+  }
+
+  doSomething () {
+    this.balance = "Doing something ...";
+  }
+}
